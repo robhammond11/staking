0b73460367029ab3df75929309c4ecece5188098
Robert Hammond <83303080+robhammond11@users.noreply.github.com>
2023-05-24 21:23:14 -0400

    staking dapp

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xd7EA09229Eb0BA38Cb38014f12A0bEC3C8B5E51E";
+export const tokenContractAddress =
+  "0x7A0A90f7a59CEa19666d3beA971DB62b3b347524";
+export const stakingContractAddress =
+  "0xe9A88E934eAc32eaf9f697311c43d4831475914c";
